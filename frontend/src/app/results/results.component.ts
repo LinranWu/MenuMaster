afa8b33d18c1b34216e50c8dbff307041966c705
LinranWu <wulinran20@gmail.com>
2023-10-17 14:04:33 -0400

    user input displayed on the result page

--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent  {
+  searchQuery: string = '';
 
+  constructor(private route: ActivatedRoute) { 
+    this.route.queryParams.subscribe(params => {
+      this.searchQuery = params['query'];
+      // Now, this.searchQuery contains the search query passed from the search component
+    });
+    console.log('Search Query:', this.searchQuery);
+
+  }
 }
