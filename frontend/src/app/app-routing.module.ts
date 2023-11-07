0ae0b6e147e56a988ba2e08c866c043a6690d7f5
Shashank Reddy <reddy.333@buckeyemail.osu.edu>
2023-11-07 15:06:04 -0500

    changes to UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResultsComponent } from './results/results.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'results/:searchQuery', component: ResultsComponent},
+  {path: 'about-us', component: AboutUsComponent},
 ];
 
 @NgModule({
